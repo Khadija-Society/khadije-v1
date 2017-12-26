2d743d3e39fa1eebca52c6590029acce06aa6565
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-12-26 15:31:18 +0330

    fixed untracked files

--- includes/option/option.me.php
+++
@@ -1,6 +0,0 @@
-<?php
-self::$config['https']                        = false;
-self::$config['redirect_url']                 = 'http://khadije.dev';
-self::$config['default_tld']                  = 'dev';
-self::$config['main_site']                    = 'http://khadije.dev';
-?>
