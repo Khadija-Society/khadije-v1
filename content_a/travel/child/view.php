d56b6019f22aec7643909b8e21be2745535e7a2b
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-12-30 11:57:18 +0330

    fix title of pages

--- content_a/travel/child/view.php
+++ content_a/travel/child/view.php
@@ -7,8 +7,9 @@ class view extends \content_a\main\view
 	public function config()
 	{
 		$this->data->page['title']   = T_("Khadije Dashboard");
-		$this->data->page['desc']    = T_("Glance at your stores and quickly navigate to stores.");
-		$this->data->page['special'] = true;
+		$this->data->page['desc']    = $this->data->site['desc'];
+		// $this->data->page['desc']    = T_("Glance at your stores and quickly navigate to stores.");
+		// $this->data->page['special'] = true;
 
 		$this->data->page['badge']['link'] = $this->url('baseFull'). '/child';
 		$this->data->page['badge']['text'] = T_('Add new child');
