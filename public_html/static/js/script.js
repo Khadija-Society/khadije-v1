51d2eb7664cd510c410a50a6d60952bf1c8f527c
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-12-31 19:40:22 +0330

    fix auto calc total price

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -5,7 +5,7 @@ route('*', function()
 
 }).once(function()
 {
-
+	bindFunctions();
 });
 
 $(function()
@@ -14,9 +14,36 @@ $(function()
 });
 
 
-
-
-function setDependImage()
+function bindFunctions()
 {
+	$('.donateProduct input[data-price]').on('input', function()
+	{
+		calcDonateProductTotalPrices();
+	});
+}
 
+
+
+function calcDonateProductTotalPrices()
+{
+	var TotalPrice = 0;
+	$('.donateProduct input[data-price]').each(function()
+	{
+		var thisProductPrice = $(this).attr('data-price');
+    	if(isNaN(thisProductPrice))
+    	{
+    		thisProductPrice = 0;
+    	}
+
+		var thisProductTotal = $(this).val() * thisProductPrice;
+    	if(isNaN(thisProductPrice))
+    	{
+    		thisProductTotal = 0;
+    	}
+    	// $(this).parents('.input').find('.addon').text(fitNumber(thisProductTotal));
+    	TotalPrice += thisProductTotal;
+
+	});
+
+	$('#totalAmount').val(TotalPrice);
 }
