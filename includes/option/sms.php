634e149dc02e30165d5a4e35ea473d40a42ba607
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-01-03 01:33:46 +0330

    add sms api key

--- includes/option/sms.php
+++ includes/option/sms.php
@@ -2,10 +2,10 @@
 /**
 * sms kavenegar config
 */
-self::$sms['kavenegar']['status'] = false;
-self::$sms['kavenegar']['apikey'] = null;
+self::$sms['kavenegar']['status'] = true;
+self::$sms['kavenegar']['apikey'] = '774F6B6C305A36356A534A6B62324D727842454D4B686558314E7145546B512B';
 self::$sms['kavenegar']['debug']  = null;
-self::$sms['kavenegar']['line']   = null;
+self::$sms['kavenegar']['line']   = '10002000200251';
 self::$sms['kavenegar']['iran']   = true;
 self::$sms['kavenegar']['header'] = null;
 self::$sms['kavenegar']['footer'] = 'khadije';
