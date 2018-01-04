e83092c4195604407323bc7e4d7c515851bfce46
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-01-04 16:20:43 +0330

    add design of group options

--- content_cp/trip/options/view.php
+++ content_cp/trip/options/view.php
@@ -16,7 +16,7 @@ class view extends \content_cp\main2\view
 		$this->data->bodyclass          = 'unselectable siftal';
 
 		$this->data->active_city        = \lib\app\travel::active_city();
-		$this->data->trip_master_active      = \lib\app\travel::trip_master_active();
+		$this->data->trip_master_active = \lib\app\travel::trip_master_active();
 		$this->data->trip_count_partner = \lib\app\travel::trip_count_partner();
 		$this->data->trip_max_awaiting  = \lib\app\travel::trip_max_awaiting();
 		$this->data->trip_getdate       = \lib\app\travel::trip_getdate();
