c678d6e03f6bc2636b583ca110522e72051d97a9
Reza <rm.biqarar@gmail.com>
2018-01-22 18:35:02 +0330

    show msg in doners

--- content/doners/view.php
+++ content/doners/view.php
@@ -14,6 +14,25 @@ class view extends \mvc\view
 
 		$this->data->DonersList = \lib\db\mytransactions::transaction_list();
 
+
+		if(\lib\session::get('payment_request_start'))
+		{
+			if(\lib\utility\payment\verify::get_status())
+			{
+				$amount = \lib\utility\payment\verify::get_amount();
+				$this->data->payment_verify_msg_true = true;
+				$this->data->payment_verify_msg = T_("Thanks for your holy payment, :amount sucsessfully recived", ['amount' => \lib\utility\human::fitNumber($amount)]);
+				\lib\app\donate::sms_success($amount);
+			}
+			else
+			{
+				$this->data->payment_verify_msg_true = false;
+				$this->data->payment_verify_msg = T_("Payment unsuccessfull");
+			}
+
+			\lib\utility\payment\verify::clear_session();
+		}
+
 		if(isset($this->controller->pagnation))
 		{
 			$this->data->pagnation = $this->controller->pagnation_get();
