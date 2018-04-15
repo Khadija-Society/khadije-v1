278500e76611a928191c1528d4f88611f2ae62b3
Reza <rm.biqarar@gmail.com>
2018-04-15 13:45:52 +0430

    fix khadem page

--- content/khadem/view.php
+++ content/khadem/view.php
@@ -2,18 +2,15 @@
 namespace content\khadem;
 
 
-class view extends \mvc\view
+class view
 {
-	public function config()
+	public static function config()
 	{
-		$this->data->page['title'] = T_("Register for new service request");
-		$this->data->page['desc']  = T_('in 3 simple step register your request for have service to holy places');
-
-		$this->data->page['badge']['link'] = \dash\url::here(). '/service';
-		$this->data->page['badge']['text'] = T_('check your service requests');
-
-		$this->data->service_need_list = \lib\db\needs::get(['type' => 'expertise', 'status' => 'enable']);
-
+		\dash\data::page_title(T_("Register for new service request"));
+		\dash\data::page_desc(T_('in 3 simple step register your request for have service to holy places'));
+		\dash\data::badge_link(\dash\url::here(). '/service');
+		\dash\data::badge_text(T_('check your service requests'));
+		\dash\data::serviceNeed_list(\lib\db\needs::get(['type' => 'expertise', 'status' => 'enable']));
 	}
 }
 ?>
