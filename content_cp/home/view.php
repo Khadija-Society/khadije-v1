43380ebe1998a1823ebe657d4ecf8b6b4561114a
Reza <rm.biqarar@gmail.com>
2018-04-18 15:05:38 +0430

    fix cp

---
+++ content_cp/home/view.php
@@ -0,0 +1,23 @@
+<?php
+namespace content_cp\home;
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::bodyclass('siftal');
+
+		\dash\data::display_cp_posts("content_cp/posts/layout.html");
+		\dash\data::display_cpSample("content_cp/sample/layout.html");
+
+
+		\dash\data::dash_version(\dash\engine\version::get());
+		\dash\data::dash_lastUpdate(\dash\utility\git::getLastUpdate());
+
+		\dash\data::page_title(T_(ucfirst( str_replace('/', ' ', \dash\url::directory()))));
+
+		// $this->data->dir['right']     = $this->global->direction == 'rtl'? 'left':  'right';
+		// $this->data->dir['left']      = $this->global->direction == 'rtl'? 'right': 'left';
+	}
+}
+?>
