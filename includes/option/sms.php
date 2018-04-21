1a39516fc5621d0c0f1795756f4eed04a6d9060c
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-04-21 19:52:33 +0430

    change sms api key

--- includes/option/sms.php
+++ includes/option/sms.php
@@ -3,7 +3,7 @@
 * sms kavenegar config
 */
 self::$sms['kavenegar']['status'] = true;
-self::$sms['kavenegar']['apikey'] = '774F6B6C305A36356A534A6B62324D727842454D4B686558314E7145546B512B';
+self::$sms['kavenegar']['apikey'] = '766552497177327441634F6F6F3156746B4E4A7869784B4A56635A7269543864';
 self::$sms['kavenegar']['debug']  = null;
 self::$sms['kavenegar']['line']   = '10002000200251';
 self::$sms['kavenegar']['iran']   = true;
