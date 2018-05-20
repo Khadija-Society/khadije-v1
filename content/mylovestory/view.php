718fad49ef8e592fa2ecba9c7872da7272f25289
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-20 12:50:10 +0430

    add some detail

--- content/mylovestory/view.php
+++ content/mylovestory/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("My Love Story"));
-		\dash\data::page_desc(T_("Download"). ' '. \dash\data::page_title());
+		\dash\data::page_desc(T_("Download :title book", ['title' => \dash\data::page_title()]));
 	}
 }
 ?>
