915561c6c9601d4769773b47f16b05374c657367
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-20 11:32:44 +0430

    fix url to mylove story

--- content/mylovestory/view.php
+++ content/mylovestory/view.php
@@ -1,12 +1,12 @@
 <?php
-namespace content\book;
+namespace content\mylovestory;
 
 
 class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_("Majaraye eshge man"));
+		\dash\data::page_title(T_("My Love Story"));
 		\dash\data::page_desc(T_("Download"). ' '. \dash\data::page_title());
 	}
 }
