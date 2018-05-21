91048debfff349a40ec6dc7d63ce3fe8a2ac295e
Reza <rm.biqarar@gmail.com>
2018-05-21 19:46:08 +0430

    update delneveshte

--- content_cp/delneveshte/edit/model.php
+++ content_cp/delneveshte/edit/model.php
@@ -13,7 +13,15 @@ class model
 			return false;
 		}
 
-		$post_detail = \dash\app\comment::edit(['status' => $status], \dash\request::get('id'));
+		$update =
+		[
+			'content' => \dash\request::post('content'),
+			'author'  => \dash\request::post('author'),
+			'email'   => \dash\request::post('email'),
+			'status'  => \dash\request::post('status'),
+		];
+
+		$post_detail = \dash\db\comments::update($update, \dash\coding::decode(\dash\request::get('id')));
 
 		if(\dash\engine\process::status())
 		{
