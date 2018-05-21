45cf9c00ec7e2c60f1c38fec6a9adb875ec8a3b0
Reza <rm.biqarar@gmail.com>
2018-05-21 18:37:17 +0430

    fix save meta of comment

--- content_delneveshte/home/model.php
+++ content_delneveshte/home/model.php
@@ -6,9 +6,11 @@ class model
 {
 	public static function post()
 	{
-		$desc   = trim(\dash\request::post('desc'));
-		$title  = trim(\dash\request::post('title'));
-		$mobile = trim(\dash\request::post('mobile'));
+		$desc       = trim(\dash\request::post('desc'));
+		$title      = trim(\dash\request::post('title'));
+		$mobile_raw = trim(\dash\request::post('mobile'));
+		$mobile     = \dash\utility\filter::mobile($mobile_raw);
+		$gender     = \dash\request::post('switchGender') ? 'female' : 'male';
 
 		if(!$desc)
 		{
@@ -40,73 +42,49 @@ class model
 				\dash\notif::warn(T_("How are you?"). ":)");
 				return false;
 			}
-
 		}
 
 		// check login
+		$user_id     = null;
 		if(\dash\user::login())
 		{
 			$user_id = \dash\user::id();
-
-			// get mobile from user login session
-			$mobile = \dash\user::login('mobile');
-
-			if(!$mobile)
-			{
-				$mobile = \dash\request::post('mobile');
-			}
-
-			// get display name from user login session
-			$displayname = \dash\user::login("displayname");
-			// user not set users display name, we get display name from contact form
-			if(!$displayname)
-			{
-				$displayname = \dash\request::post("title");
-			}
-
-		}
-		else
-		{
-			// users not registered
-			$user_id     = null;
-			$displayname = \dash\request::post("title");
-			$mobile      = \dash\request::post("mobile");
 		}
 
-		/**
-		 * register user if set mobile and not register
-		 */
+
 		if($mobile && !\dash\user::login())
 		{
-			$mobile = \dash\utility\filter::mobile($mobile);
-
-			// check valid mobile
-			if($mobile)
+			// check existing mobile
+			$exists_user = \dash\db\users::get_by_mobile($mobile);
+			// register if the mobile is valid
+			if(!$exists_user || empty($exists_user))
 			{
-				// check existing mobile
-				$exists_user = \dash\db\users::get_by_mobile($mobile);
-				// register if the mobile is valid
-				if(!$exists_user || empty($exists_user))
-				{
-					// signup user by site_guest
-					$user_id = \dash\db\users::signup(['mobile' => $mobile, 'displayname' => $title]);
-					// save log by caller 'user:send:contact:register:by:mobile'
-					\dash\db\logs::set('user:send:delneveshte:register:by:mobile', $user_id);
-				}
-				elseif(isset($exists_user['id']))
-				{
-					$user_id = $exists_user['id'];
-				}
+				// signup user by site_guest
+				$user_id = \dash\db\users::signup(['mobile' => $mobile, 'displayname' => $title]);
+				// save log by caller 'user:send:contact:register:by:mobile'
+				\dash\db\logs::set('user:send:delneveshte:register:by:mobile', $user_id);
 			}
+			elseif(isset($exists_user['id']))
+			{
+				$user_id = $exists_user['id'];
+			}
+
 		}
 
-		// ready to insert comments
+		$meta           = [];
+		$meta['gender'] = $gender;
+		$meta['mobile'] = $mobile;
+		$meta['name']   = $title;
+		$meta = json_encode($meta, JSON_UNESCAPED_UNICODE);
+
 		$args =
 		[
 			'author'  => $title,
 			'type'    => 'delneveshte',
 			'content' => $desc,
-			'user_id' => $user_id
+			'user_id' => $user_id,
+			'meta'    => $meta,
+
 		];
 		// insert comments
 		$result = \dash\db\comments::insert($args);
