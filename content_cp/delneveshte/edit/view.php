d137c2777cbc6bceb7725befdc4059aeadea3100
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-23 15:28:56 +0430

    fix edit delneveshte

--- content_cp/delneveshte/edit/view.php
+++ content_cp/delneveshte/edit/view.php
@@ -11,18 +11,19 @@ class view
 		$detail = \dash\app\comment::get($id);
 		if(!$detail)
 		{
-			\dash\header::status(403, T_("Invalid id"));
+			\dash\header::status(404, T_("Invalid id"));
 		}
 
 		\dash\data::dataRow($detail);
 		// $this->data->cat_list              = \dash\app\term::cat_list();
 
 		\dash\data::page_title(T_("Edit delneveshte"));
-		\dash\data::page_desc(T_("You can accept or reject the delneveshte"));
+		\dash\data::page_desc(T_("You can edit delneveshte if needed."). ' '. T_("This is often useful when you notice that a commenter has made a typographical error."));
 
 		\dash\data::badge_link(\dash\url::this());
-		\dash\data::badge_text(T_('Back to list of delneveshte'));
+		\dash\data::badge_text(T_('Back to list of delneveshteha'));
 
+		\dash\data::page_pictogram('commenting');
 	}
 }
 ?>
