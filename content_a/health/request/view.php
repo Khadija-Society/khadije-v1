5320ee8742bd380fd5cfddc3ce5a5d20edb68c6d
Reza <rm.biqarar@gmail.com>
2018-05-23 18:45:32 +0430

    update trasn

--- content_a/health/request/view.php
+++ content_a/health/request/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("Register for new health request"). ' | '. T_('Step 1'));
-		\dash\data::page_desc(T_('in 3 simple step register your request for have health to holy places'));
+		\dash\data::page_desc(T_('in 3 simple step register your request for have health'));
 
 		\dash\data::badge_link(\dash\url::here(). '/health');
 		\dash\data::badge_text(T_('check your health requests'));
--- content_a/representation/request/view.php
+++ content_a/representation/request/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("Register for new representation request"). ' | '. T_('Step 1'));
-		\dash\data::page_desc(T_('in 3 simple step register your request for have representation to holy places'));
+		\dash\data::page_desc(T_('in 3 simple step register your request for have representation'));
 
 		\dash\data::badge_link(\dash\url::here(). '/representation');
 		\dash\data::badge_text(T_('check your representation requests'));
