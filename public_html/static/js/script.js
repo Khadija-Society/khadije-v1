b82b01af5003abc95ddf1acac50a6de5324a1e53
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-05-23 18:22:48 +0430

    comment unused js

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,53 +1,45 @@
 
+// function pushState()
+// {
+// 	bindFunctions();
+// };
 
-route('*', function()
-{
 
-}).once(function()
-{
-	bindFunctions();
-});
+// function bindFunctions()
+// {
+//  $('.donateProduct input[data-price]').on('input', function()
+//  {
+//    calcDonateProductTotalPrices();
+//  });
+// }
+
+
+// function calcDonateProductTotalPrices()
+// {
+//  var TotalPrice = 0;
+//  $('.donateProduct input[data-price]').each(function()
+//  {
+//    var thisProductPrice = $(this).attr('data-price');
+//      if(isNaN(thisProductPrice))
+//      {
+//        thisProductPrice = 0;
+//      }
+//    var thisProductTotal = $(this).val() * thisProductPrice;
+//      if(isNaN(thisProductPrice))
+//      {
+//        thisProductTotal = 0;
+//      }
+//      // $(this).parents('.input').find('.addon').text(fitNumber(thisProductTotal));
+//      TotalPrice += thisProductTotal;
+//  });
+//  $('#totalAmount').val(TotalPrice);
+// }
+
 
 $(function()
 {
 	responsiveSiteSidebar();
 });
-
-
-function bindFunctions()
-{
-	$('.donateProduct input[data-price]').on('input', function()
-	{
-		calcDonateProductTotalPrices();
-	});
-}
-
-
-
-function calcDonateProductTotalPrices()
-{
-	var TotalPrice = 0;
-	$('.donateProduct input[data-price]').each(function()
-	{
-		var thisProductPrice = $(this).attr('data-price');
-    	if(isNaN(thisProductPrice))
-    	{
-    		thisProductPrice = 0;
-    	}
-
-		var thisProductTotal = $(this).val() * thisProductPrice;
-    	if(isNaN(thisProductPrice))
-    	{
-    		thisProductTotal = 0;
-    	}
-    	// $(this).parents('.input').find('.addon').text(fitNumber(thisProductTotal));
-    	TotalPrice += thisProductTotal;
-
-	});
-
-	$('#totalAmount').val(TotalPrice);
-}
-
 
 function responsiveSiteSidebar()
 {
