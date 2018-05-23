54735a880e74c9484092827db3330eb06f99075e
Reza <rm.biqarar@gmail.com>
2018-05-23 14:22:50 +0430

    add first amchart4

--- includes/lib/app/report.php
+++ includes/lib/app/report.php
@@ -4,6 +4,34 @@ namespace lib\app;
 
 class report
 {
+	public static function counttransaction()
+	{
+		$result  = [];
+		$query   = "SELECT sum(transactions.plus) AS `sum`, transactions.payment AS `payment` FROM transactions GROUP BY transactions.payment";
+		$payment = \dash\db::get($query, ['payment', 'sum']);
+
+		foreach ($payment as $key => $value)
+		{
+			$hazine_query = "SELECT sum(transactions.plus) AS `value`, transactions.hazinekard AS `title` FROM transactions  where transactions.payment = '$key' GROUP BY transactions.hazinekard";
+			$pie          = \dash\db::get($hazine_query, ['title', 'value']);
+			$new_pie      = [];
+
+			foreach ($pie as $title => $xvalue)
+			{
+				$new_pie[] = ['title' => $title, 'value' => $xvalue];
+			}
+
+			$result[] =
+			[
+				'payment' => T_($key),
+				'sum'     => $value,
+				'pie'     => $new_pie,
+			];
+		}
+		return $result;
+	}
+
+
 	public static function key_value($_data, $_json = false)
 	{
 		if(!is_array($_data))
