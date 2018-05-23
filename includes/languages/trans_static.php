02178006afa4ca62bacd7fbc7934a76ffbd05c36
Reza <rm.biqarar@gmail.com>
2018-05-23 17:45:05 +0430

    update trans static

---
+++ includes/languages/trans_static.php
@@ -0,0 +1,99 @@
+<?php
+class TransStatic
+{
+ private function trans_static()
+ {
+	echo T_("Khadije");
+
+	echo T_(" ");
+
+	echo T_("money");
+	echo T_("toman");
+
+	echo T_("Enable");
+	echo T_("Disable");
+	echo T_("Expire");
+	echo T_("Deleted");
+	echo T_("Lock");
+	echo T_("Awaiting");
+	echo T_("Cancel");
+	echo T_("Accept");
+	echo T_("Reject");
+	echo T_("Spam");
+	echo T_("enable");
+	echo T_("disable");
+	echo T_("expire");
+	echo T_("deleted");
+	echo T_("lock");
+	echo T_("awaiting");
+	echo T_("cancel");
+	echo T_("accept");
+	echo T_("reject");
+	echo T_("spam");
+
+	echo T_("mashhad");
+	echo T_("karbala");
+	echo T_("qom");
+	echo T_("najaf");
+	echo T_("Najaf");
+	echo T_("Mecca");
+	echo T_("mecca");
+	echo T_("syria");
+	echo T_("Syria");
+	echo T_("Group");
+
+
+	echo T_("ControlPanel");
+	echo T_("cpTripEdit");
+	echo T_("cpTripView");
+	echo T_("cpTripOption");
+	echo T_("donateManualPay");
+	echo T_("cpDonateView");
+	echo T_("cpDonateTotalPay");
+	echo T_("cpDonateOption");
+	echo T_("cpNationalCodeEdit");
+	echo T_("cpNationalCodeView");
+	echo T_("cpNationalCodeImport");
+	echo T_("cpHomePageNumber");
+	echo T_("cpOptionProduct");
+	echo T_("cpOptionCityPlace");
+	echo T_("cpServiceView");
+	echo T_("cpServiceOption");
+	echo T_("cpTripQom");
+	echo T_("cpTripMashhad");
+	echo T_("cpTripKarbala");
+	echo T_("Control");
+	echo T_("Trip");
+	echo T_("Donate");
+	echo T_("National");
+	echo T_("National Code");
+	echo T_("Setting");
+	echo T_("Service");
+
+	echo T_("cpBookTransactionView");
+	echo T_("mylovestory");
+
+	echo T_("cpReportDaily");
+	echo T_("cpReportMonth");
+	echo T_("cpReportView");
+
+	echo T_("Delneveshte");
+	echo T_("Admin Contact");
+	echo T_("Representation");
+	echo T_("Advice");
+	echo T_("Health");
+	echo T_("cpDelneveshteEdit");
+	echo T_("cpDelneveshteView");
+	echo T_("cpAdminContactChangeStatus");
+	echo T_("cpAdminContactView");
+	echo T_("cpRepresentationChangeStatus");
+	echo T_("cpRepresentationView");
+	echo T_("cpRepresentationOption");
+	echo T_("cpAdviceView");
+	echo T_("cpAdviceOption");
+	echo T_("cpHealthView");
+	echo T_("cpHealthOption");
+
+ }
+}
+?>
