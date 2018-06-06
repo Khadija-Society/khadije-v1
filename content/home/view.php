85890b4310e00536c183c6760a45f8b88f58b3bc
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-06 11:24:01 +0430

    clear some files

--- content/home/view.php
+++ content/home/view.php
@@ -5,17 +5,7 @@ class view
 {
 	public static function config()
 	{
-		if(\dash\request::get('fixallbirthdate') && \dash\permission::supervisor())
-		{
-			self::fixallbirthdate();
-		}
-
-		\dash\data::bodyclass('unselectable');
-
-		self::set_static_titles();
-
-		$url = root. 'public_html/files/data/staticvar.txt';
-
+		$url    = root. 'public_html/files/data/staticvar.txt';
 		$result = @\dash\file::read($url);
 
 		$temp            = [];
@@ -31,97 +21,6 @@ class view
 			$temp['karbala'] = isset($result[2]) ? $result[2] : null;
 		}
 		\dash\data::staticvar($temp);
-	}
-
-
-	/**
-	 * set title of static pages in project
-	 */
-	private static function set_static_titles()
-	{
-		\dash\data::page_desc(\dash\data::site_desc());
-
-		// return;
-
-		switch (\dash\url::module())
-		{
-			case '':
-			case null:
-				\dash\data::page_title(\dash\data::site_title(). ' | '. \dash\data::site_desc());
-				\dash\data::page_special(true);
-				break;
-
-
-			case 'faq':
-				\dash\data::page_title(T_('Frequently Asked Questions'));
-				\dash\data::page_desc(T_('This FAQ provides answers to basic questions about Khadije.'));
-				break;
-
-
-			case 'mission':
-				\dash\data::page_title(T_('Our missions'));
-				// \dash\data::page_desc(\dash\data::site_desc());
-				break;
-
-			case 'vision':
-				\dash\data::page_title(T_('Vision'));
-				// \dash\data::page_desc(\dash\data::site_desc());
-				break;
-
-			case 'honors':
-				\dash\data::page_title(T_('Our honors'));
-				// \dash\data::page_desc(\dash\data::site_desc());
-				break;
-
-			case 'certificate':
-				\dash\data::page_title(T_('Our certificates'));
-				// \dash\data::page_desc(\dash\data::site_desc());
-				break;
-
-			case 'about':
-				\dash\data::page_title(T_('About our charity'));
-				// \dash\data::page_desc(\dash\data::site_desc());
-				break;
-
-			case 'mahdi-imani':
-				\dash\data::page_title(T_('Shahid Mahdi Imani'));
-				\dash\data::page_desc('وصیت نامه شهید مدافع حرم، مهدی ایمانی');
-				break;
-
-			default:
-
-				break;
-		}
-	}
-
-
-
-	public static function fixallbirthdate()
-	{
-		$query = "SELECT users.id, users.birthday FROM users where users.birthday is not null";
-		$result = \dash\db::get($query, ['id', 'birthday']);
-		$update = [];
-		foreach ($result as $key => $value)
-		{
-			$new_birthdate = \dash\date::birthdate($value);
-			if(!$new_birthdate)
-			{
-				$update[] = "UPDATE users SET users.birthday = NULL WHERE users.id = $key ";
-			}
-			else
-			{
-				$update[] = "UPDATE users SET users.birthday = '$new_birthdate' WHERE users.id = $key ";
-			}
-
-		}
-		if(!empty($update))
-		{
-			\dash\db::query(implode($update, " ; "), true, ['multi_query' => true]);
-
-		}
-		echo count($update). " birthday changed";
-		\dash\code::exit();
-
 	}
 }
 ?>
--- content/view.php
+++ content/view.php
@@ -5,37 +5,14 @@ class view
 {
 	public static function config()
 	{
-
 		\dash\data::site_title(T_("Khadije Charity"));
-		// if(\dash\url::isLocal())
-		// {
-		// 		\dash\data::site_title(T_("Test"));
-		// }
 		\dash\data::site_desc(T_("Executor of first pilgrimage to the Ahl al-Bayt"));
-
 		\dash\data::site_slogan(\dash\data::site_desc());
 
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
 
-		\dash\data::bodyclass(null);
-
 		// for pushstate of main page
-		\dash\data::template_xhr('content/main/layout-xhr.html');
-		\dash\data::display_admin('content_a/main/layout.html');
-		\dash\data::template_social('content/template/social.html');
-		\dash\data::template_share('content/template/share.html');
-
-
-		// if(\dash\url::content() === null)
-		// {
-		// 	// get total uses
-		// 	$total_users                     = 10; // intval(\lib\db\userteams::total_userteam());
-		// 	$total_users                     = number_format($total_users);
-		// 	$total_users         = \dash\utility\human::number($total_users);
-		// 	\dash\data::footerStat(T_("We help :count people to work beter!", ['count' => $total_users]));
-		// }
-
-		// if you need to set a class for body element in html add in this value
+		// \dash\data::template_xhr('content/main/layout-xhr.html');
 	}
 }
 ?>
