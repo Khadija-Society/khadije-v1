8fca262d3a448ce518aa12588d285f02dc8693b8
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-08 12:28:46 +0430

    fix khadem and add live

---
+++ content/live/view.php
@@ -0,0 +1,14 @@
+<?php
+namespace content\live;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("Live broadcast of ahl-al bayt shrine"));
+		\dash\data::page_desc(T_('View live camera of ahl-al bayt shrine and have virtual pilgrimage'));
+
+	}
+}
+?>
