14b6209fd785d1e856b1ab9a780a6678e11b6d22
Reza <rm.biqarar@gmail.com>
2018-06-09 08:15:54 +0430

    add mytransaction page

---
+++ content_cp/mytransaction/view.php
@@ -0,0 +1,20 @@
+<?php
+namespace content_cp\mytransaction;
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('cpDonateTotalPay');
+		\dash\data::page_pictogram('card');
+
+		\dash\data::totalPaid(\dash\app\transaction::total_paid());
+		// \dash\data::totalPaidDate(\dash\app\transaction::total_paid_date(date("Y-m-d")));
+		// \dash\data::totalPaidCount(\dash\app\transaction::total_paid_count());
+
+		\dash\data::page_title(T_("Khadije Transaction Dashboard"));
+		\dash\data::page_desc(T_("Khadije Transaction Dashboard"));
+
+	}
+}
+?>
