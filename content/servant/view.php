6f0718721e0f24db4549e750c80c8e183c7e0bc4
Reza <rm.biqarar@gmail.com>
2018-06-10 15:57:00 +0430

    fix show khadem list

--- content/servant/view.php
+++ content/servant/view.php
@@ -9,7 +9,7 @@ class view
 		\dash\data::page_title(T_("Register for new service request"));
 		\dash\data::page_desc(T_('in 3 simple step register your request for have service to holy places'));
 
-		\dash\data::serviceNeedList(\lib\db\needs::get(['type' => 'expertise', 'status' => 'enable']));
+		\dash\data::serviceNeedList(\lib\app\need::active_list('expertise'));
 	}
 }
 ?>
