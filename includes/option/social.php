5cc9d12d07ab72e21f02b74fe1cbf5e3cc80b41f
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-13 16:53:53 +0430

    add social networks link

--- includes/option/social.php
+++ includes/option/social.php
@@ -6,8 +6,8 @@
 self::$social['status']                                = true;
 
 self::$social['list']['telegram']                      = 'mhkhadije';
-self::$social['list']['facebook']                      = null;
-self::$social['list']['twitter']                       = null;
+self::$social['list']['facebook']                      = 'mhkhadije';
+self::$social['list']['twitter']                       = 'mhkhadije';
 self::$social['list']['instagram']                     = 'mhkhadije';
 self::$social['list']['googleplus']                    = null;
 self::$social['list']['github']                        = null;
