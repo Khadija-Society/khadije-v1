c4403e7a0e94410e628093b3ea6741f690192fe5
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-16 13:08:05 +0430

    fix manager link and page

--- content/manager/view.php
+++ content/manager/view.php
@@ -7,9 +7,8 @@ class view
 
 	public static function config()
 	{
-		\dash\data::page_title(T_("Contact Us"));
+		\dash\data::page_title(T_("Contact with manager"));
 		\dash\data::page_desc(T_("We do our best to improve khadije's service quality."));
-		\dash\data::bodyclass('unselectable');
 	}
 }
 ?>
