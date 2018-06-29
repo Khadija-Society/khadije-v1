8a0e08b1a25e8c87aac0b175175815a2c843e2be
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-06-29 15:49:38 +0430

    add demo festival

---
+++ content_festival/demo/view.php
@@ -0,0 +1,21 @@
+<?php
+namespace content_festival\demo;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("Heart Writing"));
+		\dash\data::page_desc(\dash\data::site_desc());
+		\dash\data::include_css(false);
+		\dash\data::include_js(false);
+
+		\dash\data::display_delneveshte("content_festival/demo/layout.html");
+		if(\dash\request::ajax())
+		{
+			\dash\data::display_delneveshte("content_festival/demo/messages.html");
+		}
+	}
+}
+?>
