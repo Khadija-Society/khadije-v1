2d3a82184a8c379a100c5ad1448b47fea55d7564
Reza <rm.biqarar@gmail.com>
2018-07-25 15:00:54 +0430

    add course of festival page

---
+++ content_fp/course/add/view.php
@@ -0,0 +1,24 @@
+<?php
+namespace content_fp\course\add;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('fpCourseAdd');
+
+		\content_fp\course\load::festival();
+
+		\dash\data::page_pictogram('plus');
+
+		\dash\data::display_courseAdd('content_fp/course/layout.html');
+
+		\dash\data::page_title(T_("Add new course"). \dash\data::currentFestival_title());
+		\dash\data::page_desc(T_("add new festival by some data and can edit it later"));
+		\dash\data::badge_link(\dash\url::here(). '/festival');
+		\dash\data::badge_text(T_('Back to festival list'));
+
+	}
+}
+?>
