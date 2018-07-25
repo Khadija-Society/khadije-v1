15a1636b67f3e1b5936b13dd34a498dc79798c92
Reza <rm.biqarar@gmail.com>
2018-07-25 12:41:38 +0430

    save festival detail fix

--- content_fp/festival/detail/model.php
+++ content_fp/festival/detail/model.php
@@ -9,14 +9,14 @@ class model
 		\dash\permission::access('fpFestivalAdd');
 
 		$post             = [];
-		$post['intro']    = isset($_POST['intro']) ? $_POST['intro']: null;
-		$post['about']    = isset($_POST['about']) ? $_POST['about']: null;
-		$post['target']   = isset($_POST['target']) ? $_POST['target']: null;
-		$post['axis']     = isset($_POST['axis']) ? $_POST['axis']: null;
-		$post['view']     = isset($_POST['view']) ? $_POST['view']: null;
-		$post['schedule'] = isset($_POST['schedule']) ? $_POST['schedule']: null;
-		$post['place']    = isset($_POST['place']) ? $_POST['place']: null;
-		$post['award']    = isset($_POST['award']) ? $_POST['award']: null;
+		if(isset($_POST['intro'])) $post['intro'] = $_POST['intro'];
+		if(isset($_POST['about'])) $post['about'] = $_POST['about'];
+		if(isset($_POST['target'])) $post['target'] = $_POST['target'];
+		if(isset($_POST['axis'])) $post['axis'] = $_POST['axis'];
+		if(isset($_POST['view'])) $post['view'] = $_POST['view'];
+		if(isset($_POST['schedule'])) $post['schedule'] = $_POST['schedule'];
+		if(isset($_POST['place'])) $post['place'] = $_POST['place'];
+		if(isset($_POST['award'])) $post['award'] = $_POST['award'];
 
 		$result           = \lib\app\festival::edit($post, \dash\request::get('id'));
 
