028f88802d828027d61e5e018bac06fb858a3489
Reza <rm.biqarar@gmail.com>
2018-07-26 12:33:52 +0430

    fix edit course

--- content_fp/course/add/view.php
+++ content_fp/course/add/view.php
@@ -10,24 +10,27 @@ class view
 
 		\content_fp\course\load::festival();
 
+		\dash\data::page_title(T_("Add new course"). ' | '. \dash\data::currentFestival_title());
+		\dash\data::page_desc(T_("add new festival by some data and can edit it later"));
+		\dash\data::badge_link(\dash\url::here(). '/course?id='. \dash\request::get('id'));
+		\dash\data::badge_text(T_('Back to course list'));
+
+
 		if(\dash\request::get('course'))
 		{
-			$load = \lib\app\festivalcourse::get(\dash\request::get('id'));
+			$load = \lib\app\festivalcourse::get(\dash\request::get('course'));
 			if(!$load)
 			{
 				\dash\header::status(404, T_("Invalid id"));
 			}
 			\dash\data::dataRow($load);
+			\dash\data::page_title(T_("Edit course"). ' | '. \dash\data::currentFestival_title() . ' | '.  \dash\data::dataRow_title());
 		}
 
 		\dash\data::page_pictogram('plus');
 
 		\dash\data::display_courseAdd('content_fp/course/layout.html');
 
-		\dash\data::page_title(T_("Add new course"). ' | '. \dash\data::currentFestival_title());
-		\dash\data::page_desc(T_("add new festival by some data and can edit it later"));
-		\dash\data::badge_link(\dash\url::here(). '/festival');
-		\dash\data::badge_text(T_('Back to festival list'));
 
 	}
 }
