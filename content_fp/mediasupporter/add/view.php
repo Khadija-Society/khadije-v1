3c45fcfece58261b38b2ea7f68f7b725c19c217a
Reza <rm.biqarar@gmail.com>
2018-07-26 14:37:30 +0430

    add media supporter

---
+++ content_fp/mediasupporter/add/view.php
@@ -0,0 +1,37 @@
+<?php
+namespace content_fp\mediasupporter\add;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('fpCourseAdd');
+
+		\content_fp\mediasupporter\load::festival();
+
+		\dash\data::page_title(T_("Add new mediasupporter"). ' | '. \dash\data::currentFestival_title());
+		\dash\data::page_desc(T_("add new festival by some data and can edit it later"));
+		\dash\data::badge_link(\dash\url::here(). '/mediasupporter?id='. \dash\request::get('id'));
+		\dash\data::badge_text(T_('Back to mediasupporter list'));
+
+
+		if(\dash\request::get('mediasupporter'))
+		{
+			$load = \lib\app\festivaldetail::get(\dash\request::get('mediasupporter'));
+			if(!$load)
+			{
+				\dash\header::status(404, T_("Invalid id"));
+			}
+			\dash\data::dataRow($load);
+			\dash\data::page_title(T_("Edit mediasupporter"). ' | '. \dash\data::currentFestival_title() . ' | '.  \dash\data::dataRow_title());
+		}
+
+		\dash\data::page_pictogram('plus');
+
+		\dash\data::display_mediasupporterAdd('content_fp/mediasupporter/layout.html');
+
+
+	}
+}
+?>
