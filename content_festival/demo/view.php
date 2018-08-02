8cdfa4471fbddc7b74950191e6d0b3d164852813
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-02 16:44:25 +0430

    start design khadije

--- content_festival/demo/view.php
+++ content_festival/demo/view.php
@@ -6,8 +6,8 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_("Heart Writing"));
-		\dash\data::page_desc(\dash\data::site_desc());
+		\dash\data::page_title(T_("جشنواره بانوی نبی پسند"));
+		\dash\data::page_desc('');
 		\dash\data::include_css(false);
 		\dash\data::include_js(false);
 
