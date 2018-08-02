d0a9a978ab28b206469618261ffd6090142ab862
Reza <rm.biqarar@gmail.com>
2018-08-02 11:41:50 +0430

    fix back link to festival dashboard

--- content_cp/festival/detail/view.php
+++ content_cp/festival/detail/view.php
@@ -13,8 +13,8 @@ class view
 		\dash\data::page_title(T_("Edit festival"). ' '. \dash\data::dataRow_title());
 
 		\dash\data::page_desc(T_("Edit festival and add some detail for this"));
-		\dash\data::badge_link(\dash\url::here(). '/festival');
-		\dash\data::badge_text(T_('Back to festival list'));
+		\dash\data::badge_link(\dash\url::here(). '/festival?id='. \dash\request::get('id'));
+		\dash\data::badge_text(T_('Back to festival dashboard'));
 
 	}
 }
