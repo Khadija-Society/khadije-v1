70a63a1dee384c04d24434dcfb025ebd2962d126
Reza <rm.biqarar@gmail.com>
2018-08-04 11:48:35 +0430

    check is complete profile and if is complete redirect to next page

--- content_a/festival/profile/view.php
+++ content_a/festival/profile/view.php
@@ -11,6 +11,12 @@ class view
 
 		\dash\data::userdetail(\dash\db\users::get(['id' => \dash\user::id(), 'limit' => 1]));
 
+		$complete_profile = \dash\data::userdetail_iscompleteprofile();
+		if(intval($complete_profile) === 1)
+		{
+			\dash\redirect::to(\dash\url::this(). '/request?'. http_build_query(\dash\request::get()));
+		}
+
 		\content_a\profile\view::static_var();
 	}
 }
