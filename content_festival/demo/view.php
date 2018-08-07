2bfdb2d57713fd57fd798f539476f749195bb0da
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-07 11:43:48 +0430

    add map and some other fix

--- content_festival/demo/view.php
+++ content_festival/demo/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("جشنواره بانوی نبی پسند"));
-		\dash\data::page_desc('');
+		\dash\data::page_desc('طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. ');
 		\dash\data::include_css(false);
 		\dash\data::include_js(false);
 
