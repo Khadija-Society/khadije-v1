117ff924e3204f9459d86c137b27d5cfb6318f88
Reza <rm.biqarar@gmail.com>
2018-08-07 16:27:17 +0430

    fix html

--- content_festival/home/view.php
+++ content_festival/home/view.php
@@ -6,6 +6,17 @@ class view
 {
 	public static function config()
 	{
+		\dash\data::page_title(T_("جشنواره بانوی نبی پسند"));
+		\dash\data::page_desc('طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد. ');
+		\dash\data::include_css(false);
+		\dash\data::include_js(false);
+
+		\dash\data::display_festival("content_festival/home/layout.html");
+		if(\dash\request::ajax())
+		{
+			\dash\data::display_festival("content_festival/home/messages.html");
+		}
+
 		$festival = \dash\data::festival();
 		$festival_id = \dash\data::festival_id();
 
