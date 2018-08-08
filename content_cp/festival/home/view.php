0699814b75028a4e4886da2a86a2f3a3e6d7875c
Reza <rm.biqarar@gmail.com>
2018-08-08 20:07:39 +0430

    show chart fix

--- content_cp/festival/home/view.php
+++ content_cp/festival/home/view.php
@@ -27,6 +27,11 @@ class view
 			\dash\data::badge_link(\dash\url::here(). '/festival');
 			\dash\data::badge_text(T_('Back to festival list'));
 			$chart = \lib\db\festivals::chart(\dash\coding::decode($id));
+
+			if($chart === '[]')
+			{
+				$chart = null;
+			}
 			\dash\data::chartData($chart);
 
 
