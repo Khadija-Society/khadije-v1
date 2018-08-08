f6c8295fc6602a6ae403042e8785d09c1accbfdb
Reza <rm.biqarar@gmail.com>
2018-08-08 16:32:04 +0430

    fix mycourse list

--- content_a/festival/request/view.php
+++ content_a/festival/request/view.php
@@ -16,7 +16,7 @@ class view
 		\dash\data::page_title(T_("Festival course list"));
 		\dash\data::page_desc(T_('You can signup in some festival course'));
 
-		\dash\data::badge_link(\dash\url::this(). '/mycourse?id='. \dash\request::get('id'));
+		\dash\data::badge_link(\dash\url::this(). '?id='. \dash\request::get('id'));
 		\dash\data::badge_text(T_('Back to my course list'));
 
 
