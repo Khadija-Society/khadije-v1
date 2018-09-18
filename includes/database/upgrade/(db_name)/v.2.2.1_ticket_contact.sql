569692283a2817bc6b7b0adcfde4e6623c350265
Reza <rm.biqarar@gmail.com>
2018-09-18 14:18:46 +0430

    fix admincontact

--- includes/database/upgrade/(db_name)/v.2.2.1_ticket_contact.sql
+++ includes/database/upgrade/(db_name)/v.2.2.1_ticket_contact.sql
@@ -1,1 +1,2 @@
 UPDATE comments set comments.type = 'ticket' where comments.type = 'contact';
+UPDATE comments set comments.type = 'ticket', comments.title = 'تماس با مدیریت' where comments.type = 'admincontact';
