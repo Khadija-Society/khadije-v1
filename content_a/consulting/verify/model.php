926df2c30cb67bcd0db56bd3879ea6e12d1d87ae
Reza <rm.biqarar@gmail.com>
2018-10-18 12:52:04 +0330

    add verify page

---
+++ content_a/consulting/verify/model.php
@@ -0,0 +1,25 @@
+<?php
+namespace content_a\consulting\verify;
+
+
+class model
+{
+
+	public static function post()
+	{
+
+		$post            = [];
+		$post['need_id'] = \dash\request::get('id');
+		$post['status']  = 'draft';
+		$post['type']    = 'consulting';
+
+		$service_id = \lib\app\service::add($post);
+
+		if(\dash\engine\process::status() && $service_id)
+		{
+			\dash\notif::ok(T_("Your consulting was saved"));
+			\dash\redirect::to(\dash\url::here(). '/consulting/profile?id='. $service_id);
+		}
+	}
+}
+?>
