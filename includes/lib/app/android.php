100024537fc5c4c57732e4a091269ca633899133
Reza <rm.biqarar@gmail.com>
2019-01-04 19:58:06 +0330

    update android detail

--- includes/lib/app/android.php
+++ includes/lib/app/android.php
@@ -6,10 +6,18 @@ class android
 	public static function detail()
 	{
 		$detail                 = [];
+
 		$detail['navigation']   = [];
 		$detail['navigation'][] = ['title' => "pay", "url" => \dash\url::kingdom(). '/donate'];
 		$detail['navigation'][] = ['title' => "home", "url" => \dash\url::kingdom()];
 		$detail['navigation'][] = ['title' => "trip", "url" => \dash\url::kingdom(). '/trip'];
+
+		$detail['app_version']                 = [];
+		$detail['app_version']['code']         = 1;
+		$detail['app_version']['title']        = "1.0";
+		$detail['app_version']['content_text'] = null;
+		$detail['app_version']['auto_hide']    = false;
+
 
 		return $detail;
 	}
