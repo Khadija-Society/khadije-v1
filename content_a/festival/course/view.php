784c58223dbdffe3430eceed0e9a56e7ef56826c
Reza <rm.biqarar@gmail.com>
2019-01-05 07:40:48 +0330

    remove old payment code

--- content_a/festival/course/view.php
+++ content_a/festival/course/view.php
@@ -6,38 +6,6 @@ class view
 {
 	public static function config()
 	{
-		// if(\dash\session::get('payment_request_start'))
-		// {
-		// 	if(\dash\utility\payment\verify::get_status())
-		// 	{
-		// 		$amount = \dash\utility\payment\verify::get_amount();
-		// 		\dash\data:: paymentVerifyMsgTrue(true);
-		// 		\dash\data:: paymentVerifyMsg(T_("You are signuped to this course"));
-
-		// 		$course_id = \dash\coding::decode(\dash\request::get('course'));
-
-		// 		\dash\session::set('singup_festival_course_id', null);
-
-		// 		if(!\content_a\festival\course\model::signup_course($course_id))
-		// 		{
-		// 			\dash\notif::error(T_("We can not register you on this course"));
-		// 		}
-		// 		else
-		// 		{
-		// 			\dash\notif::ok(T_("You are register to this course"));
-		// 			\dash\utility\payment\verify::clear_session();
-		// 			\dash\redirect::to(\dash\url::this(). '/request?'. http_build_query(['id' => \dash\request::get('id'), 'course' => \dash\request::get('course')]));
-
-		// 		}
-		// 	}
-		// 	else
-		// 	{
-		// 		\dash\data:: paymentVerifyMsgTrue(false);
-		// 		\dash\data:: paymentVerifyMsg(T_("Payment unsuccessfull"));
-		// 	}
-
-		// 	\dash\utility\payment\verify::clear_session();
-		// }
 
 		\dash\data::page_title(T_("Signup to festival course"));
 		\dash\data::page_desc(T_('You can signup in some festival course'));
