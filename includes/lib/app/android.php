0321c57e2c1c6e803d81d2b75c0cfa8e6a631c90
Reza <rm.biqarar@gmail.com>
2019-01-09 14:14:26 +0330

    set detail in function

--- includes/lib/app/android.php
+++ includes/lib/app/android.php
@@ -3,24 +3,25 @@ namespace lib\app;
 
 class android
 {
+
 	public static function detail()
 	{
-		$detail                                = [];
+		$detail = [];
 
-		$detail['navigation']                  = [];
-		$detail['navigation'][]                = ['title' => T_("Donate"), "url" => \dash\url::kingdom(). '/donate'];
-		$detail['navigation'][]                = ['title' => T_("Home"), "url" => \dash\url::kingdom(). '/app/android'];
-		$detail['navigation'][]                = ['title' => T_("Trip"), "url" => \dash\url::kingdom(). '/trip'];
-		$detail['navigation'][]                = ['title' => T_("Delneveshte"), "url" => \dash\url::kingdom(). '/delneveshte'];
-		$detail['navigation'][]                = ['title' => T_("Setting"), "url" => 'setting'];
+		self::master_detail($detail);
 
-		$detail['app_version']                 = [];
-		$detail['app_version']['code']         = 1;
-		$detail['app_version']['title']        = "1.0";
-		$detail['app_version']['content_text'] = null;
-		$detail['app_version']['auto_hide']    = false;
+		self::navigation($detail);
+
+		self::app_version($detail);
+
+		self::intro($detail);
+
+		return $detail;
+	}
 
 
+	private static function master_detail(&$detail)
+	{
 		$detail['site']                        = \dash\url::site();
 		$detail['kingdom']                     = \dash\url::kingdom();
 		$detail['domain']                      = \dash\url::domain();
@@ -28,16 +29,39 @@ class android
 		$detail['desc']                        = T_(\dash\option::config('site','desc'));
 		$detail['logo']                        = \dash\url::static(). '/images/logo.png';
 
+	}
 
+
+	private static function navigation(&$detail)
+	{
+		$detail['navigation']                  = [];
+		$detail['navigation'][]                = ['title' => T_("Donate"), "url" => \dash\url::kingdom(). '/donate'];
+		$detail['navigation'][]                = ['title' => T_("Home"), "url" => \dash\url::kingdom(). '/app/android'];
+		$detail['navigation'][]                = ['title' => T_("Trip"), "url" => \dash\url::kingdom(). '/trip'];
+		$detail['navigation'][]                = ['title' => T_("Delneveshte"), "url" => \dash\url::kingdom(). '/delneveshte'];
+		$detail['navigation'][]                = ['title' => T_("Setting"), "url" => 'setting'];
+	}
+
+
+	private static function app_version(&$detail)
+	{
+		$detail['app_version']                 = [];
+		$detail['app_version']['code']         = 1;
+		$detail['app_version']['title']        = "1.0";
+		$detail['app_version']['content_text'] = null;
+		$detail['app_version']['auto_hide']    = false;
+	}
+
+
+	private static function intro(&$detail)
+	{
 		$detail['intro']                 = [];
 		$detail['intro']['btn']          = [];
 		$detail['intro']['btn']['next']  = T_("Next");
 		$detail['intro']['btn']['back']  = T_("Back");
 		$detail['intro']['btn']['enter'] = T_("Enter");
 
-
 		$detail['intro']['slide']   = [];
-
 		$detail['intro']['slide'][] =
 		[
 			'title'      => T_("Khadije Charity"),
@@ -69,12 +93,6 @@ class android
 			'background' => '#020456',
 			'image'      => 'https://khadije.com/static/images/logo.png',
 		];
-
-
-
-
-
-		return $detail;
 	}
 }
 ?>
