3047903254d17f156157e28b3ec4b6157efce350
Reza <rm.biqarar@gmail.com>
2019-01-23 13:00:08 +0330

    fix user code

--- includes/lib/user.php
+++ includes/lib/user.php
@@ -8,9 +8,27 @@ class user
 	{
 
 		$result                = [];
-		$result['travels']     = \lib\db\travels::get_count(['user_id' => $_user_id]);
-		$result['services']    = \lib\db\services::get_count(['user_id' => $_user_id]);
-		$result['travelusers'] = \lib\db\travelusers::get_count(['user_id' => $_user_id]);
+		$result['travels']     =
+		[
+			'count'  => \lib\db\travels::get_count(['user_id' => $_user_id]),
+			'link'   => null,
+			'encode' => false,
+		];
+
+		$result['services']    =
+		[
+			'count'  => \lib\db\services::get_count(['user_id' => $_user_id]),
+			'link'   => null,
+			'encode' => false,
+		];
+
+		$result['travelusers'] =
+		[
+			'count'  => \lib\db\travelusers::get_count(['user_id' => $_user_id]),
+			'link'   => null,
+			'encode' => false,
+		];
+
 
 		return $result;
 	}
