7b1d4eb2ea99a6ee509d97928130a28f6b8a6da9
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-02-13 13:17:16 +0330

    bugfix permission

--- content_cp/service/view/view.php
+++ content_cp/service/view/view.php
@@ -6,7 +6,7 @@ class view
 {
 	public static function config()
 	{
-		\dash\permission::access('cpRepresentationChangeStatus');
+		\dash\permission::access('cpServiceChangeStatus');
 
 		\dash\data::page_title(T_("View service detail"));
 		\dash\data::page_desc(T_("check service and update status"));
