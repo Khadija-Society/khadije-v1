e5352a5acbc2293828a2b56ac73d6862cced6e9c
Reza <rm.biqarar@gmail.com>
2019-05-01 16:39:59 +0430

    fix dashboard

--- content_api/v6/smsapp/dashboard.php
+++ content_api/v6/smsapp/dashboard.php
@@ -6,7 +6,29 @@ class dashboard
 {
 	public static function get()
 	{
-		j("Dashboard");
+
+		$result               = [];
+		$result['status']     = true;
+
+		$total                = [];
+		$total['total']       = rand(1,1000);
+		$total['send']        = rand(1,1000);
+		$total['receive']     = rand(1,1000);
+
+		$thismonth            = [];
+		$thismonth['total']   = rand(1,1000);
+		$thismonth['send']    = rand(1,1000);
+		$thismonth['receive'] = rand(1,1000);
+
+		$today                = [];
+		$today['total']       = rand(1,1000);
+		$today['send']        = rand(1,1000);
+		$today['receive']     = rand(1,1000);
+
+		$result['total']      = $total;
+		$result['thismonth']  = $thismonth;
+		$result['today']      = $today;
+		return $result;
 	}
 }
 ?>
