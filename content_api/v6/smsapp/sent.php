c883a41a680cde3a2899daa8714451444bd74ad4
Reza <rm.biqarar@gmail.com>
2019-05-01 17:15:49 +0430

    sent message

--- content_api/v6/smsapp/sent.php
+++ content_api/v6/smsapp/sent.php
@@ -4,9 +4,35 @@ namespace content_api\v6\smsapp;
 
 class sent
 {
-	public static function sent()
+	public static function set()
 	{
-		j("Sent");
+		$smsid = \dash\request::post('smsid');
+		$smsid = \dash\coding::decode($smsid);
+		if(!$smsid)
+		{
+			\dash\notif::error(T_("Invalid sms id"));
+			return false;
+		}
+
+		$load = \lib\db\sms::get(['id' => $smsid, 'limit' => 1]);
+		if(!isset($load['id']) || !isset($load['sendstatus']))
+		{
+			\dash\notif::error(T_("Sms id not found"));
+			return false;
+		}
+
+		if($load['sendstatus'] === 'sendtodevice')
+		{
+			\lib\db\sms::update(['sendstatus' => 'send'], $smsid);
+			\dash\notif::ok(T_("The message set as sent message"));
+			return true;
+		}
+		else
+		{
+			\dash\notif::error(T_("This message is not ready to save as sent message"));
+			return false;
+		}
+
 	}
 }
 ?>
