00c85612b6c5b62c29f5e078a973032b47f411fd
Reza <rm.biqarar@gmail.com>
2019-05-04 14:51:12 +0430

    fix dashboard

--- content_api/v6/smsapp/dashboard.php
+++ content_api/v6/smsapp/dashboard.php
@@ -7,27 +7,30 @@ class dashboard
 	public static function get()
 	{
 
-		$result               = [];
-		$result['status']     = true;
+		$result           = [];
+		$result['status'] = \content_api\v6\smsapp\controller::status();
 
-		$total                = [];
-		$total['total']       = rand(1,1000);
-		$total['send']        = rand(1,1000);
-		$total['receive']     = rand(1,1000);
+		$day              = [];
+		$day['send']      = rand(1,1000);
+		$day['receive']   = rand(1,1000);
 
-		$thismonth            = [];
-		$thismonth['total']   = rand(1,1000);
-		$thismonth['send']    = rand(1,1000);
-		$thismonth['receive'] = rand(1,1000);
+		$week             = [];
+		$week['send']     = rand(1,1000);
+		$week['receive']  = rand(1,1000);
 
-		$today                = [];
-		$today['total']       = rand(1,1000);
-		$today['send']        = rand(1,1000);
-		$today['receive']     = rand(1,1000);
+		$month            = [];
+		$month['send']    = rand(1,1000);
+		$month['receive'] = rand(1,1000);
 
-		$result['total']      = $total;
-		$result['thismonth']  = $thismonth;
-		$result['today']      = $today;
+		$total            = [];
+		$total['send']    = rand(1,1000);
+		$total['receive'] = rand(1,1000);
+
+
+		$result['day']    = $day;
+		$result['week']   = $week;
+		$result['month']  = $month;
+		$result['total']  = $total;
 		return $result;
 	}
 }
