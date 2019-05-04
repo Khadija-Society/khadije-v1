f0aadec8a3edf1e9b2d0a8826b14afa5d86fc87f
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-04 15:47:41 +0430

    add detect fn

---
+++ includes/lib/tg/detect.php
@@ -0,0 +1,20 @@
+<?php
+namespace lib\tg;
+use \dash\social\telegram\tg as bot;
+
+
+class detect
+{
+	public static function run($_cmd)
+	{
+		if(bot::isInline())
+		{
+			// not yet
+		}
+		else
+		{
+			// survey::detector($_cmd);
+		}
+	}
+}
+?>
