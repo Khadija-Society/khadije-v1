f09b444c4b0c07d20ceb794a9c2e53484080cacf
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-05 19:47:26 +0430

    fix link

--- content_smsapp/addgroup/view.php
+++ content_smsapp/addgroup/view.php
@@ -10,10 +10,9 @@ class view
 
 		\dash\data::page_title(T_("Add new sms group"));
 		\dash\data::page_desc(T_("You cat set some group for sms"));
+
 		\dash\data::badge_link(\dash\url::here(). '/settings');
-		\dash\data::badge_text(T_('Sms group list'));
-
-
+		\dash\data::badge_text(T_('Settings'));
 	}
 }
 ?>
