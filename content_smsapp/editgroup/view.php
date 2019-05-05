3b2edc9b5beeb641d4b7fd116ac5c486d05518d6
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-05 19:26:03 +0430

    edit group

--- content_smsapp/editgroup/view.php
+++ content_smsapp/editgroup/view.php
@@ -9,8 +9,9 @@ class view
 		\dash\data::page_pictogram('server');
 		\dash\data::page_title(T_("Edit sms group"));
 		\dash\data::page_desc(T_("You cat set some group for sms"));
-		\dash\data::badge_link(\dash\url::here(). '/listgroup');
-		\dash\data::badge_text(T_('Sms group list'));
+
+		\dash\data::badge_link(\dash\url::here(). '/settings');
+		\dash\data::badge_text(T_('Settings'));
 
 	}
 }
