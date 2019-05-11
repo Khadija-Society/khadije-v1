480067d9097f6ee3fb8d0c06560f565acbc3b08a
Reza <rm.biqarar@gmail.com>
2019-05-11 16:52:43 +0430

    fix from gateway

--- content_api/v6/smsapp/queue.php
+++ content_api/v6/smsapp/queue.php
@@ -89,8 +89,8 @@ class queue
 		[
 			'recommend_id' => null,
 			'sendstatus'   => 'awaiting',
-			// 'togateway' => \dash\utility\filter::mobile(\dash\header::get('gateway')),
-			'fromgateway'  => \dash\utility\filter::mobile(\dash\header::get('gateway')),
+			'togateway' => \dash\utility\filter::mobile(\dash\header::get('gateway')),
+			// 'fromgateway'  => \dash\utility\filter::mobile(\dash\header::get('gateway')),
 			'limit'        => 10,
 		];
 
@@ -119,8 +119,8 @@ class queue
 		$get_args =
 		[
 			'sendstatus'   => 'sendtodevice',
-			// 'togateway' => \dash\utility\filter::mobile(\dash\header::get('gateway')),
-			'fromgateway'  => \dash\utility\filter::mobile(\dash\header::get('gateway')),
+			'togateway' => \dash\utility\filter::mobile(\dash\header::get('gateway')),
+			// 'fromgateway'  => \dash\utility\filter::mobile(\dash\header::get('gateway')),
 			'limit'        => 1,
 		];
 
