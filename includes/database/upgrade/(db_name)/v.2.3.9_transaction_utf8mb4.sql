8ff8cb55403a9f34c331abb973a2717659219312
Reza <rm.biqarar@gmail.com>
2019-05-13 17:37:06 +0430

    set utf8mb4 from some field in transaction

---
+++ includes/database/upgrade/(db_name)/v.2.3.9_transaction_utf8mb4.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `transactions` CHANGE `niyat` `niyat` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
+ALTER TABLE `transactions` CHANGE `hazinekard` `hazinekard` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
+ALTER TABLE `transactions` CHANGE `fullname` `fullname` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
+ALTER TABLE `transactions` CHANGE `donate` `donate` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;
