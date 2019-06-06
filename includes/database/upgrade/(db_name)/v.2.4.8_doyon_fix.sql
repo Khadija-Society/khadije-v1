418b3ad49791576f70eb347e7310f3986fe816cf
Reza <rm.biqarar@gmail.com>
2019-06-06 12:24:47 +0430

    fix old record of doyon

---
+++ includes/database/upgrade/(db_name)/v.2.4.8_doyon_fix.sql
@@ -0,0 +1,3 @@
+UPDATE doyon SET doyon.subtitle = 'برنج خارجی' WHERE doyon.type = 'fetriye' and doyon.priceone = 24000;
+UPDATE doyon SET doyon.subtitle = 'برنج ایرانی' WHERE doyon.type = 'fetriye' and doyon.priceone = 60000;
+UPDATE doyon SET doyon.subtitle = 'گندم' WHERE doyon.type = 'fetriye' and doyon.priceone = 10000;
