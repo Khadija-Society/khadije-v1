b209fab0b6cc7f60cd2afc83c3f928af6de097c8
Reza <rm.biqarar@gmail.com>
2019-06-06 14:50:14 +0430

    add some field to doyon

--- includes/database/upgrade/(db_name)/v.2.4.8_doyon_fix.sql
+++ includes/database/upgrade/(db_name)/v.2.4.8_doyon_fix.sql
@@ -1,3 +1,5 @@
 UPDATE doyon SET doyon.subtitle = 'برنج خارجی' WHERE doyon.type = 'fetriye' and doyon.priceone = 24000;
 UPDATE doyon SET doyon.subtitle = 'برنج ایرانی' WHERE doyon.type = 'fetriye' and doyon.priceone = 60000;
 UPDATE doyon SET doyon.subtitle = 'گندم' WHERE doyon.type = 'fetriye' and doyon.priceone = 10000;
+ALTER TABLE `doyon` ADD `donestatus`  enum('ok', 'cancel', 'awaiting') NULL DEFAULT 'awaiting';
+ALTER TABLE `doyon` ADD `desc`  text CHARACTER SET utf8mb4 NULL;
