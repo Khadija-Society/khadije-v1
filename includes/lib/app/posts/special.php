e712ac4a43d05d06c5f52494a1cd3a2f0dc13a2c
Reza <rm.biqarar@gmail.com>
2019-07-11 13:15:27 +0430

    add special library

---
+++ includes/lib/app/posts/special.php
@@ -0,0 +1,17 @@
+<?php
+namespace lib\app\posts;
+
+
+class special
+{
+
+	public static function list()
+	{
+		$list             = [];
+		$list['slider']   = T_("Slider");
+		return $list;
+	}
+
+
+}
+?>
