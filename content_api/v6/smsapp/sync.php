96e2fc12d51f38dfb8e9f81e1abd0e3fadd8d60d
Reza <rm.biqarar@gmail.com>
2019-08-28 18:18:38 +0430

    set not send smsapp in sync list

--- content_api/v6/smsapp/sync.php
+++ content_api/v6/smsapp/sync.php
@@ -17,6 +17,7 @@ class sync
 		$result['status']      = \content_api\v6\smsapp\controller::status();
 		$result['dashboard']   = \content_api\v6\smsapp\dashboard::get();
 		$result['queue']       = \content_api\v6\smsapp\queue::get();
+		$result['notsend']       = \content_api\v6\smsapp\notsend::get();
 		$result['smsnewsaved'] = [];
 
 		if(isset($input['smsnew']) && is_array($input['smsnew']))
