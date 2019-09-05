6e9554e2bc7335ef837b992dc81398b521334437
Reza <rm.biqarar@gmail.com>
2019-09-05 17:19:03 +0430

    show report of transaction again but in advance search

--- content_m/productdonate/home/view.php
+++ content_m/productdonate/home/view.php
@@ -19,6 +19,7 @@ class view
 			\dash\data::page_title(\dash\data::page_title(). ' | '. T_('Search for :search', ['search' => $search_string]));
 		}
 
+		$filterArgs = [];
 		$args =
 		[
 			'sort'  => \dash\request::get('sort'),
@@ -35,6 +36,12 @@ class view
 			$args['status'] = \dash\request::get('status');
 		}
 
+		if(\dash\request::get('transaction'))
+		{
+			$args['transaction_id'] = \dash\coding::decode(\dash\request::get('transaction'));
+			$filterArgs['transaction'] = \dash\request::get('transaction');
+		}
+
 
 		$sortLink  = \dash\app\sort::make_sortLink(\lib\app\productdonate::$sort_field, \dash\url::this());
 		$dataTable = \lib\app\productdonate::list(\dash\request::get('q'), $args);
@@ -42,12 +49,9 @@ class view
 		\dash\data::sortLink($sortLink);
 		\dash\data::dataTable($dataTable);
 
-		$check_empty_datatable = $args;
-		unset($check_empty_datatable['sort']);
-		unset($check_empty_datatable['order']);
 
 		// set dataFilter
-		$dataFilter = \dash\app\sort::createFilterMsg($search_string, $check_empty_datatable);
+		$dataFilter = \dash\app\sort::createFilterMsg($search_string, $filterArgs);
 		\dash\data::dataFilter($dataFilter);
 
 
