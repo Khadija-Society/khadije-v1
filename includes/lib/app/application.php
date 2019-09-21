0e28d153c4bd0b2450ee8dcf9656c176eeb80923
Javad Adib <J.Evazzadeh@live.com>
2019-09-21 16:57:24 +0430

    change order

--- includes/lib/app/application.php
+++ includes/lib/app/application.php
@@ -18,9 +18,9 @@ class application
 		$homepage              = [];
 		$homepage[]            = self::karbala_signup_link();
 		$homepage[]            = self::link2_donate();
+		$homepage[]            = self::linksServicesLine();
 		$homepage[]            = self::inapplink('news', T_("News"));
 		$homepage[]            = self::news();
-		$homepage[]            = self::linksServicesLine();
 		$homepage[]            = self::salawat();
 		$homepage[]            = self::linksAboutLine();
 		$homepage[]            = self::linksWebsiteLine();
