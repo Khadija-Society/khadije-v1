0bf059e502dc0f02746d9b901e44a927ae4ebb84
Reza <rm.biqarar@gmail.com>
2019-09-24 18:05:42 +0330

    add arbaeen nazr

---
+++ content_m/arbaeen/view.php
@@ -0,0 +1,35 @@
+<?php
+namespace content_m\arbaeen;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('cpDonateOption');
+
+		\dash\data::page_pictogram('cogs');
+
+		\dash\data::page_title('نذورات ویژه اربعین');
+		\dash\data::page_desc(T_("check and update some options on donations"));
+		\dash\data::badge_link(\dash\url::here(). '/donate');
+		\dash\data::badge_text(T_('Back to donate list'));
+		\dash\data::bodyclass('unselectable');
+		\dash\data::include_adminPanel(true);
+		\dash\data::include_css(false);
+
+		\dash\data::need(\lib\app\need::list('donatearbaeen'));
+
+		if(\dash\request::get('edit'))
+		{
+			\dash\data::editMode(true);
+			$id = \dash\request::get('edit');
+			\dash\data::productDetail(\lib\db\needs::get(['id' => $id, 'limit' => 1]));
+			if(!\dash\data::productDetail())
+			{
+				\dash\header::status(404, T_("Id not found"));
+			}
+		}
+	}
+}
+?>
