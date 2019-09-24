ed04b6f4f51460e9b5115bf2e88377ab767b234c
Reza <rm.biqarar@gmail.com>
2019-09-24 16:41:54 +0330

    add userkarbala2

---
+++ content_m/userkarbala2/report/view.php
@@ -0,0 +1,44 @@
+<?php
+namespace content_m\userkarbala2\report;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\permission::access('cpUsersKarbalaView');
+
+		\dash\data::page_pictogram('chart');
+
+		\dash\data::page_title(T_('Report'));
+		// \dash\data::page_desc(T_('Sale your product via Jibres and enjoy using integrated web base platform.'));
+
+		\dash\data::badge_text(T_('Back'));
+		\dash\data::badge_link(\dash\url::this());
+
+
+
+		$subchild = \dash\url::subchild();
+		switch ($subchild)
+		{
+			case 'provincelist':
+				$dataTable = \lib\app\karbala2user::chart_province_list();
+				\dash\data::dataTable($dataTable);
+
+				break;
+
+			case 'map':
+				\dash\data::include_highcharts(false);
+				$chartProvinceData = \lib\app\karbala2user::chart_province();
+				\dash\data::chartProvinceData($chartProvinceData);
+				break;
+
+			default:
+				$ReportDailyChart = \lib\app\karbala2user::daily_chart();
+				\dash\data::ReportDailyChart($ReportDailyChart);
+				break;
+		}
+
+	}
+}
+?>
