24f3ab46ce939de937f8cfcb769db79efe3bc20a
Javad Adib <J.Evazzadeh@live.com>
2019-09-28 18:44:38 +0330

    fix homepage

--- content_mokeb/home/view.php
+++ content_mokeb/home/view.php
@@ -7,6 +7,15 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title("سامانه مدیریت اقامت کوتاه مدت در موکب");
+
+		$args =
+		[
+			'pagenation' => false,
+			'status' => 'enable',
+
+		];
+		$list = \lib\app\place::list(null, $args);
+		\dash\data::mokebList($list);
 	}
 }
 ?>
