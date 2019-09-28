130f873c75dac20b63b23af90797ea1cdf45d28b
Reza <rm.biqarar@gmail.com>
2019-09-28 13:47:05 +0330

    add mokeb users signup

---
+++ content_mokeb/home/controller.php
@@ -0,0 +1,13 @@
+<?php
+namespace content_mokeb\home;
+
+
+class controller
+{
+	public static function routing()
+	{
+		\dash\utility\hive::set();
+
+	}
+}
+?>
---
+++ includes/lib/db/mokebusers.php
@@ -0,0 +1,169 @@
+<?php
+namespace lib\db;
+
+class mokebusers
+{
+
+	/**
+	 * insert new productprice
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public static function insert()
+	{
+		\dash\db\config::public_insert('mokebusers', ...func_get_args());
+		return \dash\db::insert_id();
+	}
+
+
+	public static function chart_province()
+	{
+		$query = "SELECT COUNT(*) AS `count`, mokebusers.province FROM mokebusers GROUP BY mokebusers.province ORDER BY COUNT(*) DESC";
+		$result = \dash\db::get($query, ['province', 'count']);
+		return $result;
+	}
+
+	public static function daily_chart()
+	{
+		$query = "SELECT COUNT(*) AS `count`, DATE(mokebusers.datecreated) AS `date` FROM mokebusers GROUP BY DATE(mokebusers.datecreated) ORDER BY DATE(mokebusers.datecreated) ASC";
+		$result = \dash\db::get($query);
+		return $result;
+	}
+
+
+	/**
+	 * update productprice
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public static function update()
+	{
+		return \dash\db\config::public_update('mokebusers', ...func_get_args());
+	}
+
+	public static function get_last_id()
+	{
+		$query = "SELECT mokebusers.id AS `id` FROM mokebusers ORDER BY mokebusers.id DESC LIMIT 1";
+		$result = \dash\db::get($query, 'id', true);
+		return intval($result);
+	}
+
+
+	/**
+	 * get productprice detail
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public static function get()
+	{
+		$result = \dash\db\config::public_get('mokebusers', ...func_get_args());
+		return $result;
+	}
+
+
+	public static function get_sort($_where)
+	{
+		$limit = null;
+		$only_one_value = false;
+		if(isset($_where['limit']))
+		{
+			if($_where['limit'] === 1)
+			{
+				$only_one_value = true;
+			}
+
+			$limit = " LIMIT $_where[limit] ";
+		}
+
+		unset($_where['limit']);
+
+		$where = \dash\db\config::make_where($_where);
+		if($where)
+		{
+			$query = "SELECT * FROM mokebusers WHERE $where ORDER BY mokebusers.sort ASC, mokebusers.id ASC $limit";
+			$result = \dash\db::get($query, null, $only_one_value);
+			return $result;
+		}
+		return false;
+
+	}
+
+
+	/**
+	 * Searches for the first match.
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	public static function search($_string = null, $_option = [])
+	{
+		$search_field =
+		"
+			(
+				mokebusers.nationalcode = '__string__' OR
+				mokebusers.phone = '__string__' OR
+				mokebusers.firstname LIKE '__string__%' OR
+				mokebusers.lastname LIKE '__string__%' OR
+				mokebusers.displayname = '__string__%'
+			)
+		";
+
+		$mobile = \dash\utility\filter::mobile($_string);
+		if($mobile || is_numeric($_string))
+		{
+			$search_field =
+			"
+				(
+					mokebusers.mobile = '$mobile' OR
+					mokebusers.nationalcode = '__string__' OR
+					mokebusers.phone = '__string__'
+				)
+			";
+		}
+		else
+		{
+			$search_field =
+			"
+				(
+					mokebusers.firstname LIKE '__string__%' OR
+					mokebusers.lastname LIKE '__string__%' OR
+					mokebusers.displayname = '__string__%'
+				)
+			";
+		}
+
+		$default_option =
+		[
+			'search_field' => $search_field,
+		];
+
+		if(!is_array($_option))
+		{
+			$_option = [];
+		}
+
+		$_option = array_merge($default_option, $_option);
+
+		$result = \dash\db\config::public_search('mokebusers', $_string, $_option);
+		return $result;
+	}
+
+
+	/**
+	 * delete by where
+	 *
+	 * @param      <type>   $_where  The where
+	 *
+	 * @return     boolean  ( description_of_the_return_value )
+	 */
+	public static function delete_where($_where)
+	{
+		$where = \dash\db\config::make_where($_where);
+		if($where)
+		{
+			$query = "DELETE FROM mokebusers WHERE $where";
+			return \dash\db::query($query);
+		}
+		return false;
+	}
+}
+?>
