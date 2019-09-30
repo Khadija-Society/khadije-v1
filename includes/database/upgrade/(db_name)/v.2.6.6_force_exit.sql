d58731dcda485a857844e3d518469e25ec7b63b6
Reza <rm.biqarar@gmail.com>
2019-09-30 16:34:44 +0330

    add whitout postiosn fix

--- includes/database/upgrade/(db_name)/v.2.6.6_force_exit.sql
+++ includes/database/upgrade/(db_name)/v.2.6.6_force_exit.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `mokebusers` ADD `forceexit` bit(1) NULL DEFAULT NULL;
 ALTER TABLE `mokebusers` ADD `forceexitdate` datetime NULL DEFAULT NULL;
 ALTER TABLE `mokebusers` ADD `oldposition` int(10) UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE `mokebusers` ADD `noposition` bit(1) NULL DEFAULT NULL;
+ALTER TABLE `mokebusers` ADD `noposition` varchar(10) NULL DEFAULT NULL;
 
