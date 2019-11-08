b650a24dd15c0a0328a59717d89131f439bdc7c6
Reza <rm.biqarar@gmail.com>
2019-11-08 20:22:02 +0330

    add options to place

--- includes/database/upgrade/(db_name)/v.2.6.8_mokebuserspermission.sql
+++ includes/database/upgrade/(db_name)/v.2.6.8_mokebuserspermission.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `place` ADD `perm` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+ALTER TABLE `place` ADD `options` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
