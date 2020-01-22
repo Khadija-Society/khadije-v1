c68f69a16938cb5339440b2bc339b38038e10acd
Reza <rm.biqarar@gmail.com>
2020-01-22 13:52:43 +0330

    add report

---
+++ content_a/servant/report/controller.php
@@ -0,0 +1,12 @@
+<?php
+namespace content_a\servant\report;
+
+
+class controller
+{
+	public static function routing()
+	{
+		\content_agent\send\load::load();
+	}
+}
+?>
---
+++ content_a/servant/report/view.php
@@ -0,0 +1,34 @@
+<?php
+namespace content_a\servant\report;
+
+
+class view
+{
+	public static function config()
+	{
+		// \dash\permission::access('agentServantProfileView');
+		\dash\data::page_title("گزارش تصویری");
+
+		\dash\data::page_pictogram('picture');
+
+		\dash\data::badge_link(\dash\url::this(). \dash\data::xCityStart());
+		\dash\data::badge_text(T_('Back'));
+
+		$send_id = \dash\coding::decode(\dash\request::get('id'));
+		if($send_id)
+		{
+			$data = \lib\db\agentfile::get(['send_id' => $send_id]);
+			if($data)
+			{
+				$data = array_map(['\\dash\\app', 'ready'], $data);
+			}
+
+			\dash\data::dataTable($data);
+		}
+
+
+	}
+
+
+}
+?>
