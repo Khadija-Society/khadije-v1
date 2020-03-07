495e7f0fe6b149d3333e4110070956b1fac9c7d6
Reza <rm.biqarar@gmail.com>
2020-03-07 20:39:28 +0330

    change varchart to text

---
+++ includes/database/upgrade/(db_name)/v.2.8.0_lottery_user_file.sql
@@ -0,0 +1,13 @@
+
+ALTER TABLE `lottery_user` CHANGE `imagefile1` `imagefile1` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `imagefile2` `imagefile2` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `imagefile3` `imagefile3` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `imagefile4` `imagefile4` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `imagefile5` `imagefile5` TEXT null default null;
+
+ALTER TABLE `lottery_user` CHANGE `videofile1` `videofile1` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `videofile2` `videofile2` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `videofile3` `videofile3` TEXT null default null;
+ALTER TABLE `lottery_user` CHANGE `videofile4` `videofile4` TEXT null default null;
+
+
