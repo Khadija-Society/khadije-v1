48451b80ae997857a7b40e702f073fefc90a9ab9
Reza <rm.biqarar@gmail.com>
2020-03-07 19:50:51 +0330

    fix title

--- content/l/view.php
+++ content/l/view.php
@@ -6,7 +6,7 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::page_title(T_("Lottery"));
+		\dash\data::page_title("مسابقات");
 
 		$active_lottery = \lib\app\syslottery::active_list();
 		\dash\data::myList($active_lottery);
