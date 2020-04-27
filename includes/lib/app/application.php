fb79fe99f6666c56d62c917eb3cc5d98c7f78bd5
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-04-27 17:59:34 +0430

    fix

--- includes/lib/app/application.php
+++ includes/lib/app/application.php
@@ -219,7 +219,7 @@ class application
 		$link['link'][0]['url']   = \dash\url::kingdom(). '/donate';
 		$link['link'][0]['text']  = T_('Donate');
 
-		$link['link'][1]['image'] = \dash\url::static(). '/images/app/doyon-wide.jpg';
+		$link['link'][1]['image'] = \dash\url::static(). '/images/app/doyon-sharee-wide.jpg';
 		$link['link'][1]['url']   = \dash\url::kingdom(). '/doyon';
 		$link['link'][1]['text']  = T_('Donate Product');
 
