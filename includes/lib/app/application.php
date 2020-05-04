bcf6ef6b31dbc4d4fbe5a3101a1650f41f555c8e
Reza <rm.biqarar@gmail.com>
2020-05-04 14:53:47 +0430

    fix

--- includes/lib/app/application.php
+++ includes/lib/app/application.php
@@ -160,7 +160,9 @@ class application
 		// $link['type']  = 'browser';
 		$link['target'] = 'browser';
 		$link['image'] = \dash\url::static(). '/images/app/banner-ketabkhani99.jpg';
-		$link['url']   = \dash\url::kingdom(). '/race-ramezan-99';
+		//$link['url']   = \dash\url::kingdom(). '/race-ramezan-99';
+		$link['url']   = 'https://sarshomar.com/fa/s/3L32G/ex';
+
 		return $link;
 	}
 
