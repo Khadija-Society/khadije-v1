717526f09a2b0066d1e2d02bca06d1057eb59faa
Reza <rm.biqarar@gmail.com>
2020-08-31 12:54:17 +0430

    Show last protection agent

--- content_protection/home/view.php
+++ content_protection/home/view.php
@@ -7,6 +7,15 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("System manage protection users"));
+
+		$args =
+		[
+			'sort'       => 'id',
+			'order'      => 'desc'
+		];
+		$dataTable = \lib\app\protectagent::list(null, $args);
+		\dash\data::lastAgent($dataTable);
+
 	}
 
 }
