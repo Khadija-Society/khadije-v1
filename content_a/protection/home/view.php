e4b404ef20bbe13cdff85a4c1e1e6602d84e5680
Reza <rm.biqarar@gmail.com>
2020-09-03 09:23:16 +0430

    fix signup occasion

--- content_a/protection/home/view.php
+++ content_a/protection/home/view.php
@@ -9,10 +9,30 @@ class view
 		\dash\data::page_title(T_("Protection user detail"));
 		\dash\data::badge_link(\dash\url::here());
 		\dash\data::badge_text(T_('Back to dashboard'));
+
 		$occasion = \lib\app\occasion::get_active_list();
+		if(!is_array($occasion))
+		{
+			$occasion = [];
+		}
 		\dash\data::occasionList($occasion);
 
+
 		$registeredOccasion = \lib\app\protectionagentoccasion::old_registered_occasion();
+
+		$occasionID = [];
+
+		if(is_array($registeredOccasion))
+		{
+			$occasionID = array_column($registeredOccasion, 'protection_occasion_id');
+		}
+		\dash\data::occasionID($occasionID);
+
+		if(count($occasion) > count($occasionID))
+		{
+			\dash\data::haveUnregisteredOccasion(true);
+		}
+
 		\dash\data::registeredOccasion($registeredOccasion);
 
 		if(!$registeredOccasion && $occasion)
--- content_a/protection/signup/view.php
+++ content_a/protection/signup/view.php
@@ -7,7 +7,7 @@ class view
 	public static function config()
 	{
 		\dash\data::page_title(T_("Protection user detail"));
-		\dash\data::badge_link(\dash\url::here());
+		\dash\data::badge_link(\dash\url::this());
 		\dash\data::badge_text(T_('Back to dashboard'));
 		$occasion = \lib\app\occasion::get_active_list();
 		\dash\data::occasionList($occasion);
