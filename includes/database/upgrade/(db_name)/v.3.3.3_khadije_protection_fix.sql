9bead640521f9ad622dc6f039301428637aa5be3
Reza <rm.biqarar@gmail.com>
2022-01-10 12:08:48 +0330

    Add table

---
+++ includes/database/upgrade/(db_name)/v.3.3.3_khadije_protection_fix.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `protection_agent_occasion_child` (
+`id` int UNSIGNED NOT NULL auto_increment,
+`protection_occasion_id` int UNSIGNED NOT NULL,
+`protection_agent_id` int UNSIGNED NOT NULL,
+`user_id` int UNSIGNED NOT NULL,
+`capacity` int  NULL DEFAULT NULL,
+`desc` text  NULL DEFAULT NULL,
+`datemodified` datetime  NULL DEFAULT NULL,
+`datecreated` datetime  NULL DEFAULT NULL,
+PRIMARY KEY (`id`),
+CONSTRAINT `protection_agent_occasion_child_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `protection_agent_occasion_child_id` FOREIGN KEY (`protection_occasion_id`) REFERENCES `protection_occasion` (`id`) ON UPDATE CASCADE,
+CONSTRAINT `protection_agent_occasion_child_agent_id` FOREIGN KEY (`protection_agent_id`) REFERENCES `protection_agent` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
